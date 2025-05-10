b67860a331e751a322ab15480193d7ddb7464587
RoshansWorks <work.roshan.c@gmail.com>
2025-05-10 12:33:46 +0530

    updates

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000") // ✅ React frontend origin
+                    .allowedOrigins("http://localhost:3000","https://roshansworks.github.io/mis_invoice/") // ✅ React frontend origin
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
